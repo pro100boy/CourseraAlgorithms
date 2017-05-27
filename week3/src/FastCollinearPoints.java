3cb63e76488e73a842001794841f6bea9b606a9a
Pavel <gpg@ua.fm>
2017-05-27 14:24:18 +0300

    week 3 minor refactoring

--- week3/src/FastCollinearPoints.java
+++ week3/src/FastCollinearPoints.java
@@ -1,66 +1,77 @@
 import java.util.*;
 
 public class FastCollinearPoints {
-    private LineSegment[] segments;
-
+    //private LineSegment[] segments;
+    private HashMap<Double, List<Point>> foundSegments = new HashMap<>();
+    private List<LineSegment> segments = new ArrayList<>();
     // finds all line segments containing 4 or more points
-    public FastCollinearPoints(Point[] points) {
+    public FastCollinearPoints(final Point[] points) {
         Objects.requireNonNull(points);
         Arrays.stream(points).forEach(p -> Objects.requireNonNull(p));
         checkRepeatedPoints(points);
-
-        List<LineSegment> segmentsList = new LinkedList<>();
         Point[] pointsCopy = Arrays.copyOf(points, points.length);
 
         for (Point startPoint : points) {
-            // origin point
             Arrays.sort(pointsCopy, startPoint.slopeOrder());
-            Map<Double, LinkedList<Point>> map = new HashMap<>();
 
-            for (int j = 1; j < pointsCopy.length; j++) {
-                Point q = pointsCopy[j];
-                //if (q.compareTo(startPoint) == 0) continue;
+            List<Point> slopePoints = new ArrayList<>();
+            double slope = 0;
+            double previousSlope = Double.NEGATIVE_INFINITY;
 
-                // adding points into map according to their slopes
-                double d = q.slopeTo(startPoint);
-                LinkedList<Point> tmpList = map.getOrDefault(d, new LinkedList<>());
-                tmpList.add(q);
-                map.put(d, tmpList);
+            for (int i = 1; i < pointsCopy.length; i++) {
+                slope = startPoint.slopeTo(pointsCopy[i]);
+                if (slope == previousSlope) {
+                    slopePoints.add(pointsCopy[i]);
+                } else {
+                    if (slopePoints.size() >= 3) {
+                        slopePoints.add(startPoint);
+                        addSegmentIfNew(slopePoints, previousSlope);
+                    }
+                    slopePoints.clear();
+                    slopePoints.add(pointsCopy[i]);
+                }
+                previousSlope = slope;
             }
 
-            // determine list with 4+ points with same slopes
-            for (Map.Entry<Double, LinkedList<Point>> m : map.entrySet()) {
-                LinkedList<Point> tmpList = m.getValue();
-                if (tmpList.size() >= 3) {
-                    tmpList.addLast(startPoint);
-                    Collections.sort(tmpList);
-                    LineSegment lineSegment = new LineSegment(tmpList.getFirst(), tmpList.getLast());
-                    if (!containSegment(segmentsList, lineSegment)) segmentsList.add(lineSegment);
-                }
+            if (slopePoints.size() >= 3) {
+                slopePoints.add(startPoint);
+                addSegmentIfNew(slopePoints, slope);
             }
         }
-
-        segments = segmentsList.toArray(new LineSegment[segmentsList.size()]);
     }
 
-    private boolean containSegment(List<LineSegment> segmentsList, LineSegment lineSegment) {
-        boolean res = false;
-        for (LineSegment l : segmentsList)
-            if (l.toString().equals(lineSegment.toString())) {
-                res = true;
-                break;
+    private void addSegmentIfNew(List<Point> slopePoints, double slope) {
+        List<Point> endPoints = foundSegments.get(slope);
+        Collections.sort(slopePoints);
+
+        Point startPoint = slopePoints.get(0);
+        Point endPoint = slopePoints.get(slopePoints.size() - 1);
+
+        if (endPoints == null) {
+            endPoints = new ArrayList<>();
+            endPoints.add(endPoint);
+            foundSegments.put(slope, endPoints);
+            segments.add(new LineSegment(startPoint, endPoint));
+        } else {
+            for (Point currentEndPoint : endPoints) {
+                if (currentEndPoint.compareTo(endPoint) == 0) {
+                    return;
+                }
             }
-        return res;
+            endPoints.add(endPoint);
+            segments.add(new LineSegment(startPoint, endPoint));
+        }
     }
+
 
     // the number of line segments
     public int numberOfSegments() {
-        return segments.length;
+        return segments.size();
     }
 
     // the line segments
     public LineSegment[] segments() {
-        return Arrays.copyOf(segments, numberOfSegments());
+        return segments.toArray(new LineSegment[segments.size()]);
     }
 
     private void checkRepeatedPoints(Point[] points) {
